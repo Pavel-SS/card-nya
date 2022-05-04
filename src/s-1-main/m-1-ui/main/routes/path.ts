bcd176b954df604006037b30861efec387864a13
Pavel <sosnovskiy.pavel@inbox.ru>
2022-05-04 21:15:52 +0300

    renamed Page.tsx to path.ts

---
+++ src/s-1-main/m-1-ui/main/routes/path.ts
@@ -0,0 +1,10 @@
+
+export const enum PATH {
+    LOGIN = '/login/',
+    REGISTER = '/register/',
+    PROFILE = '/profile',
+    FORGOT = '/forgot-password/',
+    SET_NEW_PASS = '/set-new-password/',
+    CHECKED = '/checked-email/',
+    ERROR_404 = '/error-404'
+}
