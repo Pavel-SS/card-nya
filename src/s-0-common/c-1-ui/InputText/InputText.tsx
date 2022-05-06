d835967f3c250a8af8d5126f566bbe1bf2b0c459
Pavel <sosnovskiy.pavel@inbox.ru>
2022-05-06 21:44:27 +0300

    Create InputText.tsx

---
+++ src/s-0-common/c-1-ui/InputText/InputText.tsx
@@ -0,0 +1,56 @@
+import React, { ChangeEvent, DetailedHTMLProps, InputHTMLAttributes, KeyboardEvent, useState } from "react";
+
+//импортируем стили
+import s from './inputText.module.scss'
+
+
+//типизация стандартного input
+type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+
+//создаем свою типизацию  input, объединив стандартные (исключив type. Исключив возможность изменение типа input) со своими
+type InputTextPropsType = Omit<DefaultInputPropsType,'type'> & {
+    onChangeText?: (value: string)=> void,
+    onEnterPress?: ()=> void,
+    visibilityPassword?: boolean
+}
+
+export const InputText: React.FC<InputTextPropsType> = React.memo(({
+    onChange,
+    onChangeText,
+    onKeyPress,
+    onEnterPress,
+    visibilityPassword,
+    ...props
+}) => {
+    const [textOrPassword, setTextOrPassword] = useState<boolean>(!visibilityPassword);
+
+    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>)=>{
+        onChange && onChange(e)
+        onChangeText && onChangeText(e.currentTarget.value)
+    }
+    const onKeyPressCallback = (e: KeyboardEvent<HTMLInputElement>)=>{
+        onKeyPress && onKeyPress(e);
+        onEnterPress && e.key === 'Enter' && onEnterPress()
+    }
+
+    const hiddenPassword = () => {
+        setTextOrPassword(!textOrPassword)
+    }
+
+    return (
+        <div className={s.container}>
+            <input 
+                type={textOrPassword ? 'text' : 'password'} 
+                onChange={onChangeCallback}
+                onKeyPress={onKeyPressCallback}
+                {...props}
+            />
+            {visibilityPassword && 
+             <div 
+                onClick={hiddenPassword} 
+                className={textOrPassword ?`${s.eye} ${s.eye_open}` : `${s.eye} ${s.eye_close}`}>
+             </div>
+            }
+        </div>
+    )
+})
