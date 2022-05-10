af7d38c33af2a727877e843315d673cfa6325826
Pavel <sosnovskiy.pavel@inbox.ru>
2022-05-10 22:59:19 +0300

    Create: AddPackModal & DelPackModal

---
+++ src/s-2-features/f-5-modal_window/m-1-add_modal/AddPackModal.tsx
@@ -0,0 +1,40 @@
+import React, { useCallback, useState } from "react"
+import { useDispatch } from "react-redux"
+import { Button } from "../../../s-0-common/c-1-ui/Buttons/Button";
+import { Checkbox } from "../../../s-0-common/c-1-ui/Checkbox/Checkbox";
+import { InputText } from "../../../s-0-common/c-1-ui/InputText/InputText";
+import { addPacks } from "../../f-3-packs/p-2-bll/packsThunk";
+import { Modal, ModalType } from "../Modal"
+
+
+export const AddPackModal: React.FC<ModalType> = React.memo(({onClickModalWindow,open}) => {
+
+    const [name, setName] = useState<string>(''),
+        [privatePack,setPrivatePack] = useState<boolean>(false);
+    
+    const dispatch = useDispatch();
+
+    const onClickAddPack = useCallback(()=>{
+        dispatch(addPacks(name, privatePack))
+        onClickCleanUpState()
+    }, [dispatch, onClickModalWindow, name, privatePack])
+
+    const onClickCleanUpState = () => {
+        onClickModalWindow()
+        setName('')
+        setPrivatePack(false)
+    }
+
+    return (
+        <Modal onClickModalWindow={onClickCleanUpState} open={open}>
+            <InputText value={name} placeholder={'Enter name'}/>
+            <Checkbox checked={privatePack} onChecked={setPrivatePack}>
+                Make private
+            </Checkbox>
+            <div>
+                <Button onClick={onClickCleanUpState}>Cancel</Button>
+                <Button onClick={onClickAddPack}>Save</Button>
+            </div>
+        </Modal>
+    )
+})
