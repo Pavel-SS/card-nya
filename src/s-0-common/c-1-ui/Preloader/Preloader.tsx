62abcc165828b8f5f535aa1e989334096f4dd128
Pavel <sosnovskiy.pavel@inbox.ru>
2022-08-01 22:15:17 +0300

    Create Preloader.tsx

---
+++ src/s-0-common/c-1-ui/Preloader/Preloader.tsx
@@ -0,0 +1,11 @@
+import s from './Preloader.module.scss'
+
+export const Preloader = () => {
+    return(
+        <div className={s.progress}>
+            <div>
+
+            </div>
+        </div>
+    )
+}
