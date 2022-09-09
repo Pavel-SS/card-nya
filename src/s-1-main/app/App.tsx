67b8ae200342d2cdd21b4336c3243c809bc813b5
Pavel <sosnovskiy.pavel@inbox.ru>
2022-09-09 19:14:23 +0300

    updated

--- src/s-1-main/app/App.tsx
+++ src/s-1-main/app/App.tsx
@@ -5,20 +5,30 @@ import { RouteFunc } from '../m-1-ui/main/routes/Routes';
 import { useDispatch } from 'react-redux';
 import { authPage } from '../../s-2-features/f-2-profile/p-2-bll/profileThunk';
 import { useAppSelector } from '../m-2-bll/store';
-import { selectProfileInitialize } from '../m-2-bll/selectors';
-
+import { selectAppIsLoading, selectProfileInitialize } from '../m-2-bll/selectors';
+import { Preloader } from '../../s-0-common/c-1-ui/Preloader/Preloader';
+import s from './App.module.scss'
 
 
 function App() {
   const dispatch = useDispatch();
   const initialize = useAppSelector(selectProfileInitialize)
+  const loading = useAppSelector(selectAppIsLoading) 
+
   useEffect(() => {
     dispatch(authPage)
   },[])
 
+  // if (!initialize) {
+  //   return (
+  //     <div><Preloader/></div>
+  //   )
+  // }
+
   return (
     <div className="App">
           <HeaderMenu />
+          {/* {loading && <div className={s.appProgress}><Preloader/></div>} */}
           <RouteFunc />
     </div>
   );
