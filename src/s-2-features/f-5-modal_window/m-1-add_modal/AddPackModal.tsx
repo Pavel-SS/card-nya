f5a01f011dc82f6cbdba59ee000148fea211048b
Pavel <sosnovskiy.pavel@inbox.ru>
2022-09-10 02:03:35 +0300

    Update AddPackModal.tsx

--- src/s-2-features/f-5-modal_window/m-1-add_modal/AddPackModal.tsx
+++ src/s-2-features/f-5-modal_window/m-1-add_modal/AddPackModal.tsx
@@ -6,12 +6,15 @@ import { InputText } from "../../../s-0-common/c-1-ui/InputText/InputText";
 import { addPacks } from "../../f-3-packs/p-2-bll/packsThunk";
 import { Modal, ModalType } from "../Modal"
 import s from "./../../style/modalWindow.module.scss"
+
+
 export const AddPackModal: React.FC<ModalType> = React.memo(({onClickModalWindow,open}) => {
-    const dispatch = useDispatch();
     
     const [name, setName] = useState<string>(''),
           [privatePack, setPrivatePack] = useState<boolean>(false);
-    
+
+    const dispatch = useDispatch();
+
     const onClickAddPack = useCallback(()=>{
         dispatch(addPacks(name, privatePack))
         onClickCleanState()
@@ -25,7 +28,7 @@ export const AddPackModal: React.FC<ModalType> = React.memo(({onClickModalWindow
 
     return (
         <Modal onClickModalWindow={onClickCleanState} open={open}>
-            <InputText value={name} placeholder={'Enter name'}/>
+            <InputText value={name} placeholder={'Enter name'} onChangeText={setName}/>
             <Checkbox checked={privatePack} onChecked={setPrivatePack}>
                 Make private
             </Checkbox>
