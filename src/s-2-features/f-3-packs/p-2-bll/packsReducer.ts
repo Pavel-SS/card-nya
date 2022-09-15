6a4dbd71697c9fe628b36d8b7c2197144a6b4ace
Pavel <sosnovskiy.pavel@inbox.ru>
2022-09-15 15:12:45 +0300

    fix:  case card in packs(min/max)

--- src/s-2-features/f-3-packs/p-2-bll/packsReducer.ts
+++ src/s-2-features/f-3-packs/p-2-bll/packsReducer.ts
@@ -6,16 +6,16 @@ export const packsReducer = (state: InitPackStateType = InitPackState, action: P
     switch (action.type) {
         case 'packs/SET_PACKS':
         case 'packs/SET_COUNT_CARD_IN_PACKS':
-        case 'packs/SET_PACKS_MIN':
-        case 'packs/SET_PACKS_MAX':
+        case 'packs/SET_MIN_CARDS_COUNT':
+        case 'packs/SET_MAX_CARDS_COUNT':  
         case 'packs/SET_PACKS_STATUS':
             return {...state, ...action.payload}
         case 'packs/SET_CURRENT_PAGE':
         case 'packs/SET_SEARCH':
         case 'packs/SET_USER_PACKS':
         case 'packs/SET_SORT_PARAMETERS':
-        case 'packs/SET_MIN_CARDS_COUNT':
-        case 'packs/SET_MAX_CARDS_COUNT':
+        case 'packs/SET_PACKS_MIN':
+        case 'packs/SET_PACKS_MAX':
         case 'packs/SET_PAGE_COUNT':
             return {...state, params: {...state.params, ...action.payload}}
         default:
