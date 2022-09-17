3fd03e5a0f857fc9d76967d8d04d6eff136a5a00
Pavel <sosnovskiy.pavel@inbox.ru>
2022-09-17 22:37:37 +0300

    Update Preloader.tsx

--- src/s-0-common/c-1-ui/Preloader/Preloader.tsx
+++ src/s-0-common/c-1-ui/Preloader/Preloader.tsx
@@ -2,10 +2,13 @@ import s from './Preloader.module.scss'
 
 export const Preloader = () => {
     return(
-        <div className={s.progress}>
-            <div>
-
-            </div>
+    <div className={s.spinner_box}>
+        <div className={s.configure_border1}>
+            <div className={s.configure_core}></div>
         </div>
+        <div className={s.configure_border2}>
+            <div className={s.configure_core}></div>
+        </div>
+    </div>
     )
 }
