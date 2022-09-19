6c4b92ab0f47e000dc0cf7075a0b7fa69fa10399
Pavel <sosnovskiy.pavel@inbox.ru>
2022-09-19 23:56:29 +0300

    update

--- src/s-2-features/f-3-packs/p-1-ui/Packs.tsx
+++ src/s-2-features/f-3-packs/p-1-ui/Packs.tsx
@@ -11,29 +11,36 @@ import { AddPackModal } from '../../f-5-modal_window/m-1-add_modal/AddPackModal'
 import { packsActions } from '../p-2-bll/packsActions'
 import { PacksTable } from './PacksTable'
 
-const typePack: GeneralOrMyPacks[] = ['My','All']
+import gnel from "../../../s-1-main/app/style/gnel.module.scss"
+import text from "../../../s-1-main/app/style/text.module.scss"
+import style from './pack.module.scss'
+
+const typePack: GeneralOrMyPacks[] = ['All','My']
 
 export const Packs = () => {
     
     const dispatch = useDispatch()
 
     const [addOpen, setAddOpen] = useState<boolean>(false)
-    const [whosePack, setWhosePack] = useState<string>(typePack[0])
-    
+   
     const userID = useAppSelector(selectProfileUserID)
     const packName = useAppSelector(selectPackName)
     const packTypeSort = useAppSelector(selectPackTypeSort)
     
-    {packTypeSort === 'All' ? 
-        dispatch(packsActions.setUserPacks('')): 
-        dispatch(packsActions.setUserPacks(userID))
-    }
+    packTypeSort === 'All' ? dispatch(packsActions.setUserPacks('')) : dispatch(packsActions.setUserPacks(userID))
+
+    const [whosePack, setWhosePack] = useState(typePack[0])
 
     const onChangeTypePacks = useCallback((myOrAll: string) => {
         setWhosePack(myOrAll as GeneralOrMyPacks)
         dispatch(packsActions.setPacksStatus(myOrAll as GeneralOrMyPacks))
-        myOrAll === 'All' ? dispatch(packsActions.setUserPacks('')): 
-        dispatch(packsActions.setUserPacks(userID))
+        if(myOrAll === 'All'){
+            dispatch(packsActions.setUserPacks('')) 
+            console.log(packsActions.setUserPacks(''))
+        } else{
+            dispatch(packsActions.setUserPacks(userID))
+            console.log(packsActions.setUserPacks(userID))
+        }
     },[dispatch, userID])
 
     const onChangeRequest = useCallback((title: string)=>{
@@ -51,24 +58,21 @@ export const Packs = () => {
 
 
     return (
-        <>
-        <AddPackModal onClickModalWindow={addPackClose} open={addOpen}/>
-        <div>
-            <p>PACK</p>
-            <Radio options={typePack} onChangeOption={onChangeTypePacks}/>
-            <DoubleRangePacks/>
+        <div className={gnel.block__profile}>
+            <AddPackModal onClickModalWindow={addPackClose} open={addOpen} />
+            <div className={style.btn_section}>
+                <Radio name={'radio'} options={typePack} value={whosePack} onChangeOption={onChangeTypePacks} />
+                <DoubleRangePacks />
+            </div>
+            <div>
+                <p>PACK</p>
+                <SearchPanel value={packName} onRechenge={onChangeRequest} placeholder={'Search'} />
+                <Button onClick={addPackOpen} className={`${gnel.btn} ${text.fs14_400} ${style.profileTable_btn}`}>
+                    Add pack
+                </Button>
+                <PacksTable/>
+            </div>
+
         </div>
-        <div>
-            <SearchPanel
-                value = {packName}
-                onRechenge = {onChangeRequest}
-                placeholder = {'Search'}
-            />
-            <Button onClick={addPackOpen}>
-                Add pack
-            </Button>
-        </div>
-        <PacksTable/>
-        </>
     )
 }
