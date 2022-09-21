20f724adb85228f6c96fddcc24ab399fe5c00a02
Pavel <sosnovskiy.pavel@inbox.ru>
2022-09-21 22:42:01 +0300

    fix get requested Card

--- src/s-2-features/f-4-cards/c-2-bll/cardsActions.ts
+++ src/s-2-features/f-4-cards/c-2-bll/cardsActions.ts
@@ -3,14 +3,14 @@ import { CardType } from './../c-3-api/cardsAPI';
 
 export const cardsActions = {
     setCards: (cards: CardType[]) => ({ type: 'cards/SET_CARDS', payload: {cards}} as const),
-    setCardsID: (id: string) => ({type: 'cards/SET_CARDS_ID', payload: {id}} as const),
-    setCardsName: (name: string) => ({type: 'cards/SET_CARDS_NAME', payload: {name}} as const),
-    setCardsCount: (cardsCount: number) => ({type: 'cards/SET_CARDS_COUNT', payload: {cardsCount}} as const),
-    setCardsPageCount: (cardsPageCount: number) => ({type: 'cards/SET_CARDS_PAGE_COUNT', payload: {cardsPageCount}} as const),
+    setCardsID: (cardsPack_id: string) => ({type: 'cards/SET_CARDS_ID', payload: {cardsPack_id}} as const),
+    setCardsName: (packName: string) => ({type: 'cards/SET_CARDS_NAME', payload: {packName}} as const),
+    setCardsCount: (cardsTotalCount: number) => ({type: 'cards/SET_CARDS_COUNT', payload: {cardsTotalCount}} as const),
+    setCardsPageCount: (pageCount: number) => ({type: 'cards/SET_CARDS_PAGE_COUNT', payload: {pageCount}} as const),
     setCurrentPage: (page: number) => ({type: 'cards/SET_CURRENT_PAGE', payload: {page}} as const),
-    setAnswer: (cardAnswer: string) => ({type: 'CARDS/SET_ANSWER_SEARCH', payload: {cardAnswer}} as const),
-    setQuestion: (cardQuestion: string) => ({type: 'CARDS/SET_QUESTION_SEARCH', payload: {cardQuestion}} as const),
-    setSortParameters: (sortCards: string) => ({type: 'CARDS/SET_SORT_PARAMETERS', payload: {sortCards}} as const)
+    setAnswer: (cardAnswer: string) => ({type: 'cards/SET_ANSWER_SEARCH', payload: {cardAnswer}} as const),
+    setQuestion: (cardQuestion: string) => ({type: 'cards/SET_QUESTION_SEARCH', payload: {cardQuestion}} as const),
+    setSortParameters: (sortCards: string) => ({type: 'cards/SET_SORT_PARAMETERS', payload: {sortCards}} as const)
 }
 
 export type CardsActionTypes = InferActionType<typeof cardsActions>
